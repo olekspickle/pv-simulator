a702c7e1436ae193591bfd0d1189a10b48ce6d1d
Oleks Pickle <aleks.work2222@gmail.com>
2022-11-04 13:03:28 +0200

    calculate load values with a function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//! The idea is to simulate PV values interaction of Solar panels and consumption.
+//!
+//! Consuption is measured by meter and then added to RabbitMQ instance.
+//! From the rmq instance PV generator then takes values of a meter
+//! and outputs summarized values.
+//!
+//! The simulation should record each second with the meter values 0-9000 Watts.
+//!
+//! The end result of a simulation is a CSV file.
+//!
+
 use deadpool_lapin::lapin::{options::BasicPublishOptions, BasicProperties};
 use deadpool_lapin::{Config, Manager, Pool, Runtime};
 use lapin::ConnectionProperties;
@@ -6,27 +17,13 @@ const DEFAULT_QUEUE: &str = "amqp://rmq:rmq@127.0.0.1:5672/%2f";
 const METER_QUEUE_NAME: &str = "meter_queue";
 const PV_QUEUE_NAME: &str = "pv_queue";
 
-/// Load data for a 24h cycle
-/// TODO: make a function where:
-/// VAL = y - (x - 14)^2 / xd
-/// before 8:45 & after 19:15 ->  y = 1.5; xd = 26
-/// and in between            ->  y = 3.5; xd = 9
-/// 
-const LOAD_BIASES: &[(u8, f32)] = &[
-    (0, 0.0),
-    (6, 0.0),
-    (8, 0.25),
-    (10, 1.75),
-    (11, 2.5),
-    (12, 2.75),
-    (14, 3.5),
-    (16, 3.0),
-    (17, 2.5),
-    (18, 1.75),
-    (20, 0.15),
-    (21, 0.0),
-    (24, 0.0),
-];
+/// Approximate load data function for a 24h cycle
+fn load(h: f32) -> f32 {
+    match h {
+        h if h < 8.75 && h > 19.25 => 3.5 - (h - 14.0).powi(2) / 9.0,
+        _ => 1.5 - (h - 14.0).powi(2) / 26.0,
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -35,6 +32,7 @@ async fn main() -> Result<(), anyhow::Error> {
         ..Default::default()
     };
     let pool = pool_cfg.create_pool(Some(Runtime::Tokio1))?;
+    // Optionally uou can set amqp address as env var
     let addr = std::env::var("AMQP_ADDR").unwrap_or_else(|_| DEFAULT_QUEUE.into());
 
     Ok(())
