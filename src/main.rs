f4e45b053c0dffcc18bbb00d597f74e0b7f606f1
Oleks Pickle <aleks.work2222@gmail.com>
2022-11-21 16:10:30 +0200

    works like a charm

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//! ### Overview
+//! # Overview
 //!
 //! The idea is to simulate **PV generator** values interaction of Solar panels
 //! and building consumption measured by a Meter.
@@ -11,23 +11,23 @@
 //!
 //! The end result of a simulation is a CSV file.
 //!
-//! ### How to run
-//!     Run `start.sh` or manually:
+//! # How to run
 //!
-//!     1. Run rabbitmq:
+//! Run `start.sh` or manually:
+//!
+//! 1. Run rabbitmq:
 //!     ```bash
 //!         docker compose up
 //!     ```
 //!
-//!     2. Run the app:
+//! 2. Run the app:
 //!     ```bash
 //!         RUST_LOG=info cargo run
 //!     ```
-//!
-//!
 pub(crate) mod meter;
 pub(crate) mod pool;
 pub(crate) mod sim;
+pub(crate) mod utils;
 
 use meter::Meter;
 use sim::Simulation;
@@ -39,9 +39,9 @@ const SECS: u32 = 86400;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
-    
+
     let sim = Simulation::new()?;
-    sim.start(Meter::new(0, 9000), 0..5).await?;
+    sim.start(Meter::new(0, 9000), 0..SECS).await?;
 
     Ok(())
 }
--- src/sim.rs
+++ src/sim.rs
@@ -7,7 +7,11 @@ use anyhow::Result;
 use futures_lite::StreamExt;
 use lapin::{options::*, types::FieldTable, BasicProperties};
 use log::{debug, info};
-use std::{fs, io::Write, time, time::Duration};
+use std::{fs, io::Write, time::Duration};
+
+/// Interval in microsecs to wait between send and receive
+/// current number will define how much time the whole simulation will take
+const DEFAULT_WAIT: Duration = Duration::from_micros(250);
 
 /// Struct that will manage the simulation loop
 #[derive(Clone, Debug)]
@@ -24,38 +28,43 @@ impl Simulation {
     /// Approximate load data function for a 24h cycle
     ///
     /// Returns: power value in kW
-    /// Created with Desmos: https://www.desmos.com/calculator/s2lfq2nftl
-    pub fn generated(h: f32) -> f32 {
-        match h {
-            h if h < 8.75 && h > 19.25 => 3.5 - (h - 14.0).powi(2) / 9.0,
-            _ => 1.5 - (h - 14.0).powi(2) / 26.0,
+    /// Created with Desmos: https://www.desmos.com/calculator/03gcro3e4d
+    pub fn generate(h: f32) -> f32 {
+        let p = match h {
+            h if h < 8.0 && h > 20.0 => 1.5 - (h - 14.0).powi(2) / 32.0,
+            _ => 3.2 - (h - 14.0).powi(2) / 11.0,
+        };
+        match p {
+            p if p < 0.0 => 0.0,
+            p => p,
         }
     }
 
     /// Starts the simulation loop with given time frame
     ///
     /// In order to run simulation in order of a few minutes
-    /// Meter values will be produced approximately 360 times per second (60Hz)
-    /// This way the whole 24h cycle will run in 240s(4m) timespan
+    /// Meter values will be produced approximately 612 times per second (~160Hz)
+    /// This way the whole 24h cycle will run in 140s(2m20s) timespan
     pub async fn start(&self, meter: Meter, time_range: std::ops::Range<u32>) -> Result<()> {
+        Self::reset_previous_results()?;
+
         let queue = self.pool.declare_queue().await?;
         info!("Declared the '{:?}' queue", queue);
 
-        // Lifetimes in threads: we need it just for creating a channel in a thread scope
-        let pool_1 = self.pool.clone();
+        // Lifetimes in threads: we need it just to manage channel in a thread scope
+        let pub_channel = self.pool.channel().await?;
         // 1. Launch meter generation process in one task
         info!("Launching meter values generation task...");
         tokio::spawn(async move {
             for n in time_range {
-                let meter_value: MeterRecord = meter.consume().into();
-                let payload = meter_value.to_string().as_bytes().to_vec();
+                let meter_record = MeterRecord::new(n, meter.consume());
+                let payload = meter_record.to_string().as_bytes().to_vec();
                 debug!(
                     "{n}-Meter payload:{:?}",
                     String::from_utf8(payload.clone()).unwrap()
                 );
 
-                let channel = pool_1.channel().await?;
-                channel
+                pub_channel
                     .basic_publish(
                         "", // exchange
                         METER_QUEUE,
@@ -71,31 +80,29 @@ impl Simulation {
             Ok::<(), anyhow::Error>(())
         });
 
-        info!("Launching PV subscriber...");
         // 2. Launch pv receiving in another task
-        let channel = self
-            .pool
-            .channel()
-            .await
-            .expect("failed to create a sub connection");
-        let mut sub = channel
-            .basic_consume(
-                METER_QUEUE,
-                "test_consume",
-                BasicConsumeOptions::default(),
-                FieldTable::default(),
-            )
-            .await
-            .expect("failed to create a subscription");
+        info!("Launching PV subscriber...");
+        let mut sub = self.consumer().await?;
+        loop {
+            // Without this workaround the process hangs at the end on the amqp async stream loop.
+            // This is what I came up with to break it gracefully, but async is still hard :(
+            // So it looks terrible but justifiable in this kind of project.
+            let sleep = tokio::time::sleep(Duration::from_secs(2));
+            tokio::pin!(sleep);
+            let delivery = tokio::select! {
+                success = sub.try_next() => Ok(success?),
+                _ = &mut sleep => Err(anyhow::anyhow!("No messages"))
+            };
 
-        Self::write_row(&mut b"timestamp,meter_value,pv_value,resulting\n".to_vec())?;
+            if delivery.is_err() {
+                break;
+            }
 
-        while let Some(delivery) = sub.next().await {
-            let delivery = delivery.expect("error in consumer");
+            let delivery = delivery?.expect("No messages");
             let record: MeterRecord = delivery.data.clone().into();
 
             let meter_value = record.value();
-            let pv_value = Self::generated(record.hours());
+            let pv_value = Self::generate(record.hours());
             let resulting = meter_value / 1000.0 - pv_value;
             debug!("{meter_value}/{} - {pv_value}={resulting}", 1000.0);
 
@@ -107,14 +114,23 @@ impl Simulation {
                 .ack(BasicAckOptions::default())
                 .await
                 .expect("ACK failed");
-
-            Self::tick().await;
-            let n = self.check_message_count().await?;
-            debug!("N:{n}");
-            if n == 0 {
-                break;
-            }
         }
+
+        info!("Done");
+
+        Ok(())
+    }
+
+    /// Truncate file and add columns to it
+    fn reset_previous_results() -> Result<()> {
+        let mut file = fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open("results.csv")
+            .unwrap();
+
+        file.write_all(b"timestamp,meter_value,pv_value,resulting\n")?;
 
         Ok(())
     }
@@ -127,12 +143,32 @@ impl Simulation {
             .create(true)
             .open("results.csv")
             .unwrap();
-        file.write_all(buf).expect("Could not write to a file");
+        file.write_all(buf)?;
 
         Ok(())
     }
 
-    async fn check_message_count(&self) -> Result<u32> {
+    /// Create a consumer with from the current pool's connection
+    async fn consumer(&self) -> Result<lapin::Consumer> {
+        let sub_channel = self
+            .pool
+            .channel()
+            .await
+            .expect("failed to create a sub connection");
+        let sub = sub_channel
+            .basic_consume(
+                METER_QUEUE,
+                "test_consume",
+                BasicConsumeOptions::default(),
+                FieldTable::default(),
+            )
+            .await
+            .expect("failed to create a subscription");
+
+        Ok(sub)
+    }
+
+    async fn _check_message_count(&self) -> Result<u32> {
         let ch = self.pool.channel().await?;
         let queue = ch
             .queue_declare(
@@ -147,7 +183,48 @@ impl Simulation {
 
     /// Wait some time
     async fn tick() {
-        let mut interval = tokio::time::interval(Duration::from_micros(250));
+        let mut interval = tokio::time::interval(DEFAULT_WAIT);
         interval.tick().await;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use expect_test::expect;
+
+    #[test]
+    fn generate_pv() {
+        // 8:30
+        let h = Simulation::generate(8.5);
+        expect!["0.45000005"].assert_eq(&h.to_string());
+
+        // 10:30
+        let h = Simulation::generate(10.5);
+        expect!["2.0863638"].assert_eq(&h.to_string());
+
+        // 12:30
+        let h = Simulation::generate(12.5);
+        expect!["2.9954545"].assert_eq(&h.to_string());
+
+        // 14:30
+        let h = Simulation::generate(14.5);
+        expect!["3.1772728"].assert_eq(&h.to_string());
+
+        // 16:42
+        let h = Simulation::generate(16.7);
+        expect!["2.5372725"].assert_eq(&h.to_string());
+
+        // 17:42
+        let h = Simulation::generate(17.7);
+        expect!["1.9554541"].assert_eq(&h.to_string());
+
+        // 18:42
+        let h = Simulation::generate(18.7);
+        expect!["1.1918175"].assert_eq(&h.to_string());
+
+        // 20:42
+        let h = Simulation::generate(20.1);
+        expect!["0"].assert_eq(&h.to_string());
     }
 }
